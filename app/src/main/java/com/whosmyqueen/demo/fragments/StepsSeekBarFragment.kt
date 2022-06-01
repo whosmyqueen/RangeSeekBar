d30269ddcdf67e77f5ee4e682f939ee541c94a2f
whosmyqueen <644049260@qq.com>
2022-06-01 15:55:24 +0800

    update 4.0.0

--- app/src/main/java/com/whosmyqueen/demo/fragments/StepsSeekBarFragment.kt
+++ app/src/main/java/com/whosmyqueen/demo/fragments/StepsSeekBarFragment.kt
@@ -1,9 +1,11 @@
 package com.whosmyqueen.demo.fragments
 
+import android.util.Log
 import android.view.View
 import com.whosmyqueen.demo.R
+import com.whosmyqueen.widget.OnRangeChangedListener
+import com.whosmyqueen.widget.RangeSeekBar
 import kotlinx.android.synthetic.main.fragment_step.*
-import java.util.ArrayList
 
 /**
 //                       _ooOoo_
@@ -34,19 +36,33 @@ import java.util.ArrayList
  * =====================================================
  */
 class StepsSeekBarFragment : BaseFragment() {
-	override fun getLayoutId(): Int {
-		return R.layout.fragment_step
-	}
+    val TAG: String = "StepsSeekBarFragment"
+    override fun getLayoutId(): Int {
+        return R.layout.fragment_step
+    }
 
-	override fun initView(view: View) {
-		val stepsDrawables = ArrayList<Int>()
-		stepsDrawables.add(R.drawable.step_1)
-		stepsDrawables.add(R.drawable.step_2)
-		stepsDrawables.add(R.drawable.step_3)
-		stepsDrawables.add(R.drawable.step_4)
-		sb_steps_1?.setStepsDrawable(stepsDrawables)
-		sb_steps_2?.setStepsDrawable(stepsDrawables)
-	}
+    override fun initView(view: View) {
+        val stepsDrawables = ArrayList<Int>()
+        stepsDrawables.add(R.drawable.step_1)
+        stepsDrawables.add(R.drawable.step_2)
+        stepsDrawables.add(R.drawable.step_3)
+        stepsDrawables.add(R.drawable.step_4)
+        sb_steps_1?.setStepsDrawable(stepsDrawables)
+        sb_steps_2?.setStepsDrawable(stepsDrawables)
+        sb_steps_7?.setOnRangeChangedListener(object : OnRangeChangedListener {
+            override fun onRangeChanged(view: RangeSeekBar?, leftValue: Float, rightValue: Float, isFromUser: Boolean) {
+                Log.e(TAG,"onRangeChanged : $leftValue")
+            }
+
+            override fun onStartTrackingTouch(view: RangeSeekBar?, isLeft: Boolean) {
+				Log.e(TAG,"onStartTrackingTouch : ${view?.rangeSeekBarState?.get(0)?.value}")
+            }
+
+            override fun onStopTrackingTouch(view: RangeSeekBar?, isLeft: Boolean) {
+				Log.e(TAG,"onStopTrackingTouch : ${view?.rangeSeekBarState?.get(0)?.value}")
+            }
+        })
+    }
 
 
 }
